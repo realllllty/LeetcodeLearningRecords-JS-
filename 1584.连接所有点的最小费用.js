95a794d6ea1abb7d507f677f34c97ead3d088f2a
LinTianyu <1747666525@163.com>
2023-12-09 00:29:49 +0800

    leetcode 300

---
+++ 1584.连接所有点的最小费用.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1584 lang=javascript
+ *
+ * [1584] 连接所有点的最小费用
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minCostConnectPoints = function(points) {
+
+};
+// @lc code=end
+
