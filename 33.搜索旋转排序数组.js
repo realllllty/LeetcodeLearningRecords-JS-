facd3120fad4593923d98150b5624fd810d457d1
LinTianyu <1747666525@163.com>
2024-03-17 11:37:47 +0800

    74. 搜索二维矩阵

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -10,10 +10,60 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  // 如何使用二分查找完成这道题???
-  // point ---- end - start --- point-1
+// var search = function(nums, target) {
+//   // 如何使用二分查找完成这道题???
+//   // point ---- end - start --- point-1
+//   /**
+//    * 在有条件的情况下===>进行二分
+//    * 什么条件?
+//    * 满足, 区间有序, 并且在这个区间范围之内
+//    */
+//   let pre = 0;
+//   let bac = nums.length - 1;
+//   let cur = ~~((pre + bac) / 2);
+//   while (pre <= bac) {
+//     if () {
+//       // 相等, 直接返回
+//     } else if (nums[pre] <= nums[index]) {
+//       // 左边有序, 两种情况
+//       if (nums[index] > target) {
+//         bac = index - 1;
+//       } else if (nums[index] < target) {
+//         pre = index + 1;
+//       }
+//     } else if (nums[index] <= nums[bac]){
+//       // 右边有序
+//     }
+//   }
   
+// };
+var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      
+      if (nums[mid] === target) {
+          return mid;
+      }
+      
+      if (nums[left] <= nums[mid]) {
+          if (nums[left] <= target && target < nums[mid]) {
+              right = mid - 1;
+          } else {
+              left = mid + 1;
+          }
+      } else {
+          if (nums[mid] < target && target <= nums[right]) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+  }
+  
+  return -1;
 };
 // @lc code=end
 
---
+++ 74.搜索二维矩阵.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=javascript
+ *
+ * [74] 搜索二维矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let flatMatrix = [];
+  let res = false;
+  // 扁平化matrix数组
+  matrix.forEach((item) => {
+    item.forEach((initem) => {
+      flatMatrix.push(initem);
+    })
+  })
+  // 对处理数组实行二分查找
+  let left = 0;
+  let right = flatMatrix.length - 1;
+  let cur = ~~((left + right)/2);
+  while (left <= right) {
+    if (flatMatrix[cur] === target) {
+      res = true;
+      break;
+    } else if (flatMatrix[cur] > target) {
+      right = cur - 1;
+    } else {
+      left = cur + 1;
+    }
+    cur = ~~((left + right)/2);
+  }
+  return res;
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = searchMatrix;
+// @after-stub-for-debug-end
