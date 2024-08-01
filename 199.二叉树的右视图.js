3482ab2488bd68e3be2637577423ad014e489c1c
lintianyu5 <lintianyu5@jd.com>
2024-08-01 11:09:16 +0800

    543

---
+++ 199.二叉树的右视图.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=javascript
+ *
+ * [199] 二叉树的右视图
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    // 层序遍历
+    
+};
+// @lc code=end
+
