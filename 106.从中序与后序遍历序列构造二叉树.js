e536e4de94e374252b5c53112e79d0d0468a2f36
LinTianyu <1747666525@163.com>
2024-09-03 00:51:44 +0800

    106

--- 106.从中序与后序遍历序列构造二叉树.js
+++ 106.从中序与后序遍历序列构造二叉树.js
@@ -18,19 +18,27 @@
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-var buildTree = function(inorder, postorder) {
+var buildTree = function (inorder, postorder) {
+    function TreeNode(val, left, right) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+
     if (!inorder.length) return null;
 
     // 先从后序遍历当中找到根节点
     let root = postorder[postorder.length - 1];
-    let rootIndex = inorder.findIndex(v => v.val === root.val);
-    
-    // 按照这个root进行切分中序遍历, slice是前闭后开, 每次都要忘
-    let rightInorder = inorder.slice(0, rootIndex);
-    let leftInorder = inorder.slice(rootIndex + 1);
+    let rootIndex = inorder.findIndex(v => v === root);
 
-    let rightPostorder = postorder.slice(0, rightInorder.length);
-    let leftPostorder = postorder.slice(rightInorder.length, postorder.length);
+    // 按照这个root进行切分中序遍历, slice是前闭后开, 每次都要忘
+    let leftInorder = inorder.slice(0, rootIndex);
+    let rightInorder = inorder.slice(rootIndex + 1);
+
+    let leftPostorder = postorder.slice(0, leftInorder.length);
+    let rightPostorder = postorder.slice(leftInorder.length, postorder.length - 1);
+
+    root = new TreeNode(root, null, null);
 
     root.left = buildTree(leftInorder, leftPostorder);
     root.right = buildTree(rightInorder, rightPostorder);
