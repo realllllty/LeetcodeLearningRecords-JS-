8dbf81df2dd815096334503db5fc0e8368487ec9
lintianyu5 <lintianyu5@jd.com>
2024-09-03 16:56:53 +0800

    300

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,32 +10,17 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  // let dp = new Array(nums.length).fill(1);
-
-  // for (let i = 0; i < nums.length; i++) {
-  //   for (let j = 0; j < i; j++) {
-  //     if (nums[i] > nums[j]) {
-  //       dp[i] = Math.max(dp[i], dp[j] + 1);
-  //     }
-  //   }
-  // }
-  // let res = 0;
-  // for (let i = 0; i < dp.length; i++) {
-  //   res = Math.max(res, dp[i]);
-  // }
-  // return res;
+  // 定义dp数组, 用于记录在当前index : i下的最长序列值是多少(默认长度为1)
+  const dp = new Array(nums.length).fill(1);
   
-  // Vue3 diff当中不仅仅像这道题一样是最长自增子序列问题, 还需要获得序列的具体值
-  dp (index, maxNum) {
-    if () {
-      
-    } else {
-      // 当当前的数字要大于最大值的时候, 就能够选择
-      if (nums[index] > maxNum) {
-        return Math.max(dp(index + 1, maxNum), dp(index + 1, nums[nums]) + 1);
-      } 
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        // 对当前的j(i-index前的index枚举), 可以选择j, 也可以不选择j, 对于每个不同的j, 都存储其最大值
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
     }
   }
-
+  return Math.max(...dp);
 };
 // @lc code=end
